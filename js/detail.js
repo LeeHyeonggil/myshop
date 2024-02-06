8a31d11d353a7ebe752fc6fe37661fe6f398e635
LeeHyeonggil <155707160+LeeHyeonggil@users.noreply.github.com>
2024-02-06 13:19:36 +0900

    상품리스트

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,10 @@
 $(function(){
     $('.img-thumb-box>img').click(function(){
-        const src = $(this).attr('.src');
-        $('.img-box>img').attr('.arc',src); 
+        const src = $(this).attr('src');
+        $('.img-box>img').attr('src',src); 
+    });
+    $('.colors input[type=radio]').click(function(){
+        const color = $(this).val();
+        $('.selected').text(color);
     });
 });
